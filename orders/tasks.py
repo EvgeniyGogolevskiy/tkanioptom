2398b534cc198ad2b2d843a2afa0182a67193813
EvgeniyGogolevskiy <111510037+EvgeniyGogolevskiy@users.noreply.github.com>
2022-10-09 15:26:28 +0300

    Update tasks.py

--- orders/tasks.py
+++ orders/tasks.py
@@ -12,7 +12,7 @@ def order_created(order_id):
     subject = f'Заказ №{order_id} оформлен'
     message = f'Уважаемый(ая) {order.first_name} Ваш заказ № {order_id} успешно оформлен!\n' \
               f'Ожидайте подтверждения по телефону.'
-    mail_sent = send_mail(subject, message, 'evgeni3197@gmail.com', [order.email], fail_silently=False)
+    mail_sent = send_mail(subject, message, 'gmail с которого будут отправляться сообщения', [order.email], fail_silently=False)
     print('OK ', order_id)
     return mail_sent
 
@@ -26,7 +26,7 @@ def order_push(order_id):
     subject = f'Некий {order.first_name} оформил заказ №{order_id}'
     message = f'Заказ № {order_id} оформлен!\n ФИО клиента {order.last_name} {order.first_name} {order.father_name}\n' \
               f'Емэйл {order.email}\n Телефон {order.number_phone}'
-    mail_sent = send_mail(subject, message, 'evgeni3197@gmail.com', ['evgeni3197@mail.ru'], fail_silently=False)
+    mail_sent = send_mail(subject, message, 'gmail с которого будут отправляться сообщения', ['Адресс на которй будут приходить сообщения'], fail_silently=False)
     print('OK2 ', order_id)
     return mail_sent
 
